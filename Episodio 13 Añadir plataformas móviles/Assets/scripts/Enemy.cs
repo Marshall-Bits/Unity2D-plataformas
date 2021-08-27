e9b0c075fa62d11bfcd0a0623b263c8a2a7db996
Marcel <69241444+Marshall-Bits@users.noreply.github.com>
2021-08-27 11:38:46 +0200

    Add files via upload

---
+++ Episodio 13 Añadir plataformas móviles/Assets/scripts/Enemy.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float enemySpeed = -1f;
+    Rigidbody2D enemyRigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(isFacingLeft())
+      {
+          enemyRigidBody.velocity = new Vector2(enemySpeed, enemyRigidBody.velocity.y);
+      }
+      else
+      {
+          enemyRigidBody.velocity = new Vector2(-enemySpeed, enemyRigidBody.velocity.y);
+      }
+    }
+
+    bool isFacingLeft()
+    {
+        return transform.localScale.x > 0;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        transform.localScale = new Vector2(enemyRigidBody.velocity.x, transform.localScale.y);
+    }
+}
+
+
+
+
---
+++ Episodio 13 Añadir plataformas móviles/Assets/scripts/MovingPlatform.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public Transform point1, point2, startingPoint;
+    public float platformSpeed = 1f;
+
+    Vector2 nextPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = startingPoint.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       float timeSteps = platformSpeed * Time.deltaTime;
+       if(transform.position == point1.position)
+       {
+           nextPosition = point2.position;
+       }
+       else if(transform.position == point2.position)
+       {
+           nextPosition = point1.position;
+       }
+
+       transform.position = Vector2.MoveTowards(transform.position, nextPosition, timeSteps);
+    }
+}
