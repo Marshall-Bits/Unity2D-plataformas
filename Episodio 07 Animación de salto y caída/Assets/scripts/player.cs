a8d838f4bfb53cbd65c2b4f900b014072a0b6997
Marcel <69241444+Marshall-Bits@users.noreply.github.com>
2021-08-27 10:57:28 +0200

    Add files via upload

---
+++ Episodio 07 Animación de salto y caída/Assets/scripts/player.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+   [SerializeField] float runSpeed = 10;
+   [SerializeField] float jumpSpeed = 5;
+   Rigidbody2D rigidbody;
+   Collider2D collider;
+   SpriteRenderer spriteRenderer;
+   Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+      rigidbody = gameObject.GetComponent<Rigidbody2D>();
+      collider = gameObject.GetComponent<Collider2D>();
+      spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+      animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         Run();  
+         Jump();
+         FlipSprite();
+         CheckGround();
+    }
+
+    private void Run()
+    {
+       var getDirection = Input.GetAxis("Horizontal");
+       rigidbody.velocity = new Vector2(getDirection * runSpeed, rigidbody.velocity.y);
+       animator.SetBool("isRunning", true);
+       if(getDirection == 0)
+       {
+         animator.SetBool("isRunning", false);
+       }
+    }
+    private void CheckGround()
+    {
+      if(collider.IsTouchingLayers(LayerMask.GetMask("ground")))
+      {
+       animator.SetBool("isGrounded", true); 
+      }
+      else
+      {
+      animator.SetBool("isGrounded", false); 
+      }
+    }
+    private void Jump()
+    {
+      animator.SetFloat("jumpVelocity",rigidbody.velocity.y);
+      if(!collider.IsTouchingLayers(LayerMask.GetMask("ground")))
+      {
+         return;
+      }
+      if(Input.GetButton("Jump"))
+      {
+         rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpSpeed);
+      }
+    }
+
+   private void FlipSprite()
+   {
+      if(rigidbody.velocity.x < 0)
+      {
+         spriteRenderer.flipX = true;
+      }
+      else if(rigidbody.velocity.x > 0)
+      {
+          spriteRenderer.flipX = false;
+      }
+   }
+}
