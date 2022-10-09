637708580e48ec458f11d000163a1157ece215de
Marcel <69241444+Marshall-Bits@users.noreply.github.com>
2022-10-09 12:14:50 +0200

    Add files via upload
    Añadido episodio 14

---
+++ Episodio 14 cambiar de escena/Assets/scripts/ExitPortal.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitPortal : MonoBehaviour
+{
+    GameObject levelTransition;
+    LevelTransition levelTransitionScript;
+
+    void Start()
+    {
+        levelTransition = GameObject.Find("LevelTransition");
+        levelTransitionScript = levelTransition.GetComponent<LevelTransition>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        levelTransitionScript.LoadNextLevel();
+    }
+}
---
+++ Episodio 14 cambiar de escena/Assets/scripts/LevelTransition.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTransition : MonoBehaviour
+{
+    Animator animator;
+
+
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    public void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex; 
+        StartCoroutine(LoadLevel(currentSceneIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        animator.SetTrigger("End");
+
+        yield return new WaitForSeconds(1);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+}
